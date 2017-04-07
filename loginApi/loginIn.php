349eab1391f9444313becfce92008a6eb78fcecf
ChaoLeer <18709270892@163.com>
2017-04-08 01:32:59 +0800

    init

---
+++ loginApi/loginIn.php
@@ -0,0 +1,16 @@
+<?php 
+  //设置编码为UTF-8
+  header("Content-Type:application/json;charset=UTF-8"); 
+  //处理跨域  
+  header("Access-Control-Allow-Origin: *"); 
+  //*号表示所有域名都可以访问  
+  header("Access-Control-Allow-Method: POST,GET");  
+  echo "login";
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+  if ($username == "lichao" && $password == "loveqin") {
+    echo "login success!";
+  } else {
+    echo "login faild!";
+  }
+?>
